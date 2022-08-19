ce61f79736b439d1d67de15f27cc7c22b1a86261
Gangyeong Lee <gang7994@naver.com>
2022-08-19 20:24:51 +0900

    클래스 정리

--- Main.py
+++ Main.py
@@ -1,11 +1,93 @@
+from lib2to3.pgen2.token import OP
 import pygame, sys
 from Button import Button
 from Settings import *
 import GameManager
-pygame.init()
 
+class Encyclopedia:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((screen_width, screen_height))
+        pygame.display.set_caption("Enc")
+    def Encyclopedia(self):
+        while True:
+            OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+            self.screen.fill("white")
+            OPTIONS_TEXT = get_font(45).render("백과사전창", True, "Black")
+            OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=((screen_width/2), 30))
+            self.screen.blit(OPTIONS_TEXT, OPTIONS_RECT)
+            OPTIONS_BACK = Button(image=None, pos=(1300, 0), 
+                                text_input="BACK", font = get_font(60), base_color="Black", hovering_color="Red")
+            OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+            OPTIONS_BACK.update(self.screen)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+                        main_menu()
+            pygame.display.update()
+
+class Credit:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((screen_width, screen_height))
+        pygame.display.set_caption("Cre")
+    def Credit(self):
+        while True:
+            OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+            self.screen.fill("white")
+            OPTIONS_TEXT = get_font(45).render("크레딧창", True, "Black")
+            OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=((screen_width/2), 30))
+            self.screen.blit(OPTIONS_TEXT, OPTIONS_RECT)
+            OPTIONS_BACK = Button(image=None, pos=(1300, 0), 
+                                text_input="BACK", font=get_font(60), base_color="Black", hovering_color="Red")
+            OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+            OPTIONS_BACK.update(self.screen)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+                        main_menu()
+            pygame.display.update()
+            
+class Option:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((screen_width, screen_height))
+        pygame.display.set_caption("Cre")
+    def Option(self):
+        while True:
+            OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+            self.screen.fill("white")
+            OPTIONS_TEXT = get_font(45).render("옵션창", True, "Black")
+            OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=((screen_width/2), 30))
+            self.screen.blit(OPTIONS_TEXT, OPTIONS_RECT)
+
+            OPTIONS_BACK = Button(image=None, pos=(1300, 0), 
+                                text_input="BACK", font=get_font(60), base_color="Black", hovering_color="Red")
+
+            OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+            OPTIONS_BACK.update(self.screen)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+                        main_menu()
+            pygame.display.update()
+
+
+pygame.init()
 SCREEN = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Menu")
+Enc = Encyclopedia()
+Cre = Credit()
+Opt = Option()
 
 def get_titlefont(size): 
     return pygame.font.Font("Images/TestPix/font.ttf", size)
@@ -17,75 +99,10 @@ def Play():
     game = GameManager.GameManager()
     game.Run()
 
-def Encyclopedia():
-    while True:
-        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
-        SCREEN.fill("white")
-        OPTIONS_TEXT = get_font(45).render("백과사전창", True, "Black")
-        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=((screen_width/2), 30))
-        SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
-        OPTIONS_BACK = Button(image=None, pos=(1300, 0), 
-                            text_input="BACK", font=get_font(60), base_color="Black", hovering_color="Red")
-        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
-        OPTIONS_BACK.update(SCREEN)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
-                    main_menu()
-        pygame.display.update()
-
-def Credit():
-    while True:
-        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
-        SCREEN.fill("white")
-        OPTIONS_TEXT = get_font(45).render("크레딧창", True, "Black")
-        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=((screen_width/2), 30))
-        SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
-        OPTIONS_BACK = Button(image=None, pos=(1300, 0), 
-                            text_input="BACK", font=get_font(60), base_color="Black", hovering_color="Red")
-        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
-        OPTIONS_BACK.update(SCREEN)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
-                    main_menu()
-        pygame.display.update()
-    
-def Option():
-    while True:
-        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
-
-        SCREEN.fill("white")
-
-        OPTIONS_TEXT = get_font(45).render("옵션창", True, "Black")
-        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=((screen_width/2), 30))
-        SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
-
-        OPTIONS_BACK = Button(image=None, pos=(1300, 0), 
-                            text_input="BACK", font=get_font(60), base_color="Black", hovering_color="Red")
-
-        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
-        OPTIONS_BACK.update(SCREEN)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
-                    main_menu()
-        pygame.display.update()
-
 def main_menu():
     SCREEN.fill(BLACK)
     while True:
         MENU_MOUSE_POS = pygame.mouse.get_pos()
-
         MENU_TEXT = get_titlefont(100).render("Mythief", True, "#b68f40")
         MENU_RECT = MENU_TEXT.get_rect(topleft=(80, 80))
 
@@ -113,11 +130,11 @@ def main_menu():
                     if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
                         Play()
                     if ENCYCLOPEDIA_BUTTON.checkForInput(MENU_MOUSE_POS):
-                        Encyclopedia()
+                        Enc.Encyclopedia()
                     if CREDIT_BUTTON.checkForInput(MENU_MOUSE_POS):
-                        Credit()
+                        Cre.Credit()
                     if OPTION_BUTTON.checkForInput(MENU_MOUSE_POS):
-                        Option()
+                        Opt.Option()
                     if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
                         pygame.quit()
                         sys.exit()
