8be7cdb5daba3abf2d007159e864982acf2a133b
AnDongH <87707867+AnDongH@users.noreply.github.com>
2022-10-04 03:45:45 +0900

    오타 수정

--- GameManager.py
+++ GameManager.py
@@ -253,7 +253,7 @@ class GameManager:
     def item_interaction_text(self):
         if self.level.player.item_interaction:
             font = pygame.font.Font("Images/TestPix/Mabinogi_Classic_TTF.ttf", 20)
-            tap_key = font.render("Tap을 누르시오", True, BLACK)
+            tap_key = font.render("Tab을 누르시오", True, BLACK)
             txt_w, txt_h = tap_key.get_size()
             self.screen.blit(self.interaction_UI,(screen_width // 2 - 149 // 2, screen_height // 2 - 75))
             self.screen.blit(tap_key, (screen_width // 2 - txt_w // 2, screen_height // 2 - 65))
